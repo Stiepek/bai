9a88f8f86a9789705d3b5c7dc823a85345683169
Piotrek <StiepanR923@gmail.com>
2020-03-22 20:55:28 +0100

    Update cars.js

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,5 +1,13 @@
 function isCar(car1, car2, car3) {
-    // return 
+    let p;
+    if(car1 == true && car2 == true && car3 == true){
+        p = true;
+    } else if(car1 == false && car2 == false && car3 == false){
+        p = true;
+    } else {
+        p = false;
+    }
+    return p;
 }
 
 console.log(isCar(true, true, true));
