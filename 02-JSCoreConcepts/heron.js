259a8f1b43d57f98320797046d083dd7d18001a9
Piotrek <StiepanR923@gmail.com>
2020-03-22 20:55:54 +0100

    Update heron.js

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -2,10 +2,16 @@
 //a = 3;
 b = 4;
 c = 5;
-h = 7
+h = 7;
+
+let s = (b + c + h)/2;
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
+var a;
+a = (x, y, z, o) => {
+    let p;
+    p = Math.sqrt(o * (o-x) * (o-y) * (o-z) );
+    return p;
+};
 
-let a = () => "aaaa";
-
-console.log( a() );
+console.log( `Pole trójkąta o bokach ${b}, ${c}, ${h} wynosi ${a(b, c, h, s)}` );
