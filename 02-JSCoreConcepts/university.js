e2c6299724c366c211184b80642213454ce105c2
Piotrek <StiepanR923@gmail.com>
2020-03-22 20:58:18 +0100

    Update university.js

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -2,23 +2,45 @@ names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        let p = this.names;
+        let o = [];
+        p.forEach(item => {
+            if(item[0] == letter){
+                o.push(item);
+            }
+        });
+        return o;// names (array) which starts with the letter
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        let p = this.names.sort();
+        console.log(p);
+        let o = [];
+        p.forEach(item => {
+            o.push(item);
+        });
+        return o;// names (array) in alphabetical order 
     }
 
     get() {
-        return // names (array) in natural order
+        let p = this.names;
+        //console.log(p);
+        return p; // names (array) in natural order
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        let p = this.names;
+        console.log(p);
+        let o = [];
+        for(let i = 0; i < n; i++){
+            //console.log(p[i]);
+            o.push(p[i]);
+        };
+        return o;// get first 'n' names (array) 
     }
 }
 
@@ -27,9 +49,10 @@ console.log(students.get());
 console.log(students.sort());
 console.log(students.startWith('P'));
 console.log(students.startWith('M').sort());
-console.log(students.sort().getFirst(4));
-console.log(students.startWith('M').sort().getFirst(2));
+//console.log(students.sort().getFirst(4));
+//console.log(students.startWith('M').sort().getFirst(2));
 console.log(students.get());
+console.log(students.getFirst(3));
 
 
 
